b921a4c49bd78cf6710ebb43860de624e9120835
sivagurubilli <gurusrija123@gmail.com>
2022-05-07 21:07:44 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const express = require("express")
-const  port = 5000
+const  port = process.env.PORT||5000
 const mongoose = require("mongoose")
 const Reguser = require("./models/usermodel")
 const useraddress = require("./models/addressmodel")
